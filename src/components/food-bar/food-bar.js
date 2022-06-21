4fc96280e5b0280335f605551bedf4468286b7f5
malcom adams <davisjamal647@gmail.com>
2022-06-21 19:47:52 +0100

    changed logo

--- src/components/food-bar/food-bar.js
+++ src/components/food-bar/food-bar.js
@@ -4,18 +4,20 @@ import style from './food-bar.module.css'
 export default function FoodBar() {
     const [food, setFood] = useState('');
     const [allFood, setAllFood] = useState([])
-    const [calories, setCalories] = useState(0)
+    const [calories, setCalories] = useState([])
     const [allCalories, setAllCalories] = useState([])
     let arr = []
 
     const handleChange = (target) => {
         const value = target.target.value;
         setFood(value)
+        
     }
 
     const handleCalories = (e) => {
         const value = e.target.value;
         setCalories(value)
+        
     }
 
     const handleFood = (e) => {
@@ -28,7 +30,7 @@ export default function FoodBar() {
         }
         setAllFood((prev) => ([...prev, food]))
         setAllCalories((cal) => ([...cal, calories]))
-        console.log(calories, calories.length)
+
     }
     
 
@@ -46,22 +48,23 @@ export default function FoodBar() {
     return (
         <div className={style["container"]}>
             <form onSubmit={handleFood}>
-                <input className={style["bar"]} required onChange={handleChange}/>
-                <input className={style["mid-bar"]} onChange={handleCalories}/>
+                <input className={style["bar"]} required onChange={handleChange} placeholder="food"/>
+                <input className={style["mid-bar"]} onChange={handleCalories} placeholder="calories"/>
                 <button className={style["add"]}>add</button>
-                {allFood ? <div className="foodlist">
+                {allFood ? <div className={style["foodlist"]}>
                     {allFood.map((item, index) => 
                         <div key={index} className={style["fooditem"]}>
                             <p value={index}>{item}</p>
                             {allCalories ? (<div key={index}>
                                 {allCalories.map((obj, ind) => <div key={ind}>
-                                    <p value={ind}>{obj}</p>
+                                    <p>{obj} kcal</p>
                                 </div>)}
                             </div>) : (<div></div>)}
                             <button className={style["del-btn"]} value={item} onClick={handleDelete}>x</button>
                         </div>
                     )}
                 </div> : <div></div>}
+
             </form>    
         </div>
     )
